481554bd0c647d634d8b23bc7259d45f5f5808c1
triplecar <767578538@qq.com>
2021-04-11 20:44:04 +0800

    init project

---
+++ gulimall-member/src/test/java/com/triplecar/gulimall/member/GulimallMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.triplecar.gulimall.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
